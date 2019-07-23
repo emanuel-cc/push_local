d9d35f010c4a218056259784a2fb261c7632dd18
Emanuel Antonio <Emanuel Antonio@DESKTOP-10ULSAH.(none)>
2019-07-23 00:48:36 -0500

    push local

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:push_local/src/pages/mensaje_page.dart';
+import 'package:push_local/src/providers/push_notifications_provider.dart';
+import 'package:push_local/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+  //Inicializamos la clase PushNotificationProvider
+  @override
+  void initState() {
+    //// TODO: implement initState
+    super.initState();
+    final pushProvider = PushNotificationProvider();
+    //Lanzamos el pushProvider
+    pushProvider.initNotifications();
+    pushProvider.mensajes.listen((data){
+      //Navigator.pushNamed(context, 'mensaje');
+      print('Argumento del push');
+      print(data);
+
+      navigatorKey.currentState.pushNamed('mensaje',arguments: data);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //Manejar el estado de material app
+      navigatorKey: navigatorKey,
+      title: 'Push Local',
+      initialRoute: 'home',
+      routes: {
+        'home' : (BuildContext context) => HomePage(),
+        'mensaje' : (BuildContext context) => MensajePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/providers/push_notifications_provider.dart
@@ -0,0 +1,74 @@
+//Ayuda a expandir la aplicacion con alguna funcionalidad (el archivo provider)
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationProvider{
+  //Inicializar las notificaciones
+  //pedirle permiso al usuario que va a recibir las notificaciones y 
+  //obtener el identificador unico o token de este dispositivo
+  // y ese token se va a guardar en la base de datos
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  //Escuchar cuando la notificacion caiga
+  final _mensajeStreamController = StreamController<String>.broadcast();
+  //Emitir mensaje
+  Stream<String> get mensajes => _mensajeStreamController.stream;
+
+  initNotifications(){
+    //Pedir permiso para utilizar esas notificaciones
+    _firebaseMessaging.requestNotificationPermissions();
+    //Obtener el token de dispositivo
+    _firebaseMessaging.getToken().then((token){
+      //Ese token que me toca almacenar en la base de datos
+      print('======== FCM token=========');
+      print(token);
+    }); //Regresa un Future, que es el token
+
+    //Configuracion
+    //Metodos a llamar con los diferentes pasos de las etapas para recibir una push
+    _firebaseMessaging.configure(
+      //Se va a disparar cuando la aplicacion esta abierta
+      onMessage: (info){
+        print('====== On Message =======');
+        print(info);
+
+        String argumento = 'no-data';
+        //Determinar cuando estas en Android o en IOS
+        if(Platform.isAndroid){
+          argumento=info['data']['comida'] ?? 'no-data';
+        }else{
+          argumento = info['comida'] ?? 'no-data-ios';
+        }
+        _mensajeStreamController.sink.add(argumento);
+      },
+      //Recibir las notificaciones
+      onLaunch: (info){
+        print('====== On Launch =======');
+        print(info);
+        
+      },
+      onResume: (info){
+        print('====== On Resume =======');
+        print(info);
+
+        String argumento = 'no-data';
+        //Determinar cuando estas en Android o en IOS
+        if(Platform.isAndroid){
+          argumento=info['data']['comida'] ?? 'no-data';
+        }else{
+          argumento = info['comida'] ?? 'no-data-ios';
+        }
+
+        _mensajeStreamController.sink.add(argumento);
+      },
+    );
+  }
+
+  //Metodo dispose
+  dispose(){
+    _mensajeStreamController?.close();
+  }
+}
